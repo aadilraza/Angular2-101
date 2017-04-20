882b43d65a6303ab6c1374ea2d372b99bc3f2018
Madhu Ranjan <madhu.ranjan@outlook.com>
2017-04-20 10:00:09 -0500

    angular in asp mvc

---
+++ Angular2InASPMVC/Angular2InASPMVC/client/app.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: '/components/app'
+})
+export class AppComponent { name = 'Angular'; }
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
